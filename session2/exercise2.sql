b674f37e09f8bc91c4cf3ab0f350cddc4ce093d9
fullhdpixel <tsmudde@hotmail.com>
2014-05-06 15:28:50 +0200

    Exercise 2.2

--- session2/exercise2.sql
+++ session2/exercise2.sql
@@ -13,3 +13,30 @@ CREATE TABLE F (
 	violation BOOLEAN,
 	motivation VARCHAR(200)
 );
+
+CREATE TABLE R1 (
+	A INT,
+	B INT,
+	C INT,
+	E INT,
+	F INT,
+);
+
+CREATE TABLE R2 (
+	E INT,
+	D INT,
+);
+
+CREATE TABLE F1 (
+	x VARCHAR(10),
+	y VARCHAR(10),
+	violation BOOLEAN,
+	motivation VARCHAR(200)
+);
+
+CREATE TABLE F2 (
+	x VARCHAR(10),
+	y VARCHAR(10),
+	violation BOOLEAN,
+	motivation VARCHAR(200)
+);
--- session2/result2.sql
+++ session2/result2.sql
@@ -1,4 +1,13 @@
-insert into F (x, y, violation, motivation) values
-	(ABC,EF,false,'BCNF is goed toegepast. Als je ABC weet, kun je daar alles van afleiden (Superkey).'),
-	(E,D,true,'E -> is overbodig, want ABC -> EFD'),
-	(D,A,true,'Uit A volgt D d.m.v. ABC -> EF, E -> D, D -> A D is not unique');
+INSERT INTO F (x, y, violation, motivation) 
+	values
+		(ABC,EF,false,'BCNF is goed toegepast. Als je ABC weet, kun je daar alles van afleiden (Superkey).'),
+		(E,D,true,'E -> is overbodig, want ABC -> EFD'),
+		(D,A,true,'Uit A volgt D d.m.v. ABC -> EF, E -> D, D -> A D is not unique');
+	
+INSERT INTO F1 (x, y, violation, motivation)
+	values
+		(ABC,EF,false,'BCNF is toegepast: geen schending');
+		
+INSERT INTO F2 (x, y, violation, motivation)
+	values
+		(E,D,false,'BCNF is toegepast: geen schending');
