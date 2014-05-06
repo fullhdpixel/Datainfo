0c300078e2a0877cd0ce27ef7996924df9d60d6c
fullhdpixel <tsmudde@hotmail.com>
2014-05-06 14:42:13 +0200

    SQL practicum

---
+++ project/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ideating</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="semantic.css" rel="stylesheet" type="text/css">
+	<link href="feed.css" rel="stylesheet" type="text/css">
+
+	<script src="jquery2.0.3.js"></script>
+	<script src="jqueryaddress.js"></script>
+	<script src="semantic.js"></script>
+	<script src="feed.js"></script>
+	
+</head>
+<body>
+	<div id='cssmenu'>
+		<ul>
+			<li><a href='#'><span style="position: float; left: 25px;">Login</span></a></li>
+			<li class='last'><a href='#'><span>Contact</span></a></li>		   
+			<li><a href='#'><span>About</span></a></li>
+			<li class='active'><a href='#'><span>Home</span></a></li>
+		</ul>
+	</div>
+	
+	<div style="position: fixed; left; 0%; top: 15%; overflow: hidden;" class="ui right floated black launch button" onclick="">
+		Menu
+	</div>
+	
+	<div id="grad1"></div>
+	
+	<div class="ui large inverted vertical sidebar menu">
+		<a class="active item">
+		  Lorem ipsum <span class="ui label">100</span>
+		</a>
+		<a class="item">
+		  Lorem ipsum <span class="ui label">100</span>
+		</a>
+		<div class="item">
+		  <b>Lorem ipsum</b>
+		  <div class="menu">
+			<a class="item">
+			  Lorem ipsum <span class="ui label">100</span>
+			</a>
+			<a class="item">
+			  Lorem ipsum <span class="ui label">100</span>
+			</a>
+		  </div>
+		</div>
+		<a class="item">
+		  <i class="bookmark icon"></i> Lorem ipsum
+		</a>
+		<div class="ui dropdown item">
+		  <i class="add icon"></i> Lorem ipsum
+		</div>
+  </div>
+</body>
+</html>
+
+<?php
+/*PHP goes here*/
+?>
---
+++ session2/result1.sql
@@ -0,0 +1,12 @@
+insert into Problem1 (task, result, motivatie) values 
+	('a',false,'Capaciteit is niet uniek.'),
+	('b',true,'Elk voertuig heeft een capaciteit.'),
+	('c',true,'Aan de hand van de factuur en het afleveradres is de pakje bekend bij De Pakjesman.'),
+	('d',false,'Tenzij het pakje gelabeled is wordt niet duidelijk a.h.v. het pakje was het afleveradres c.q. factuur is.'),
+	('e',false,'Het geldbedrag en de ontvanger zijn niet identificeerbaar a.h.v. pakje.'),
+	('f',false,'Een bezorger is niet afhankelijk van één specifiek voertuig.'),
+	('g',false,'Voertuig en capaciteit hebben geen vaste bezorger'),
+	('h',true,'B is een functionele afhankelijkheid van V'),
+	('i',true,'C is een functionele afhankelijkheid van V'),
+	('j',true,'h en i zijn waar, dus de combinatie van h en i is ook true'),
+);
