2aa2b2923976446651ffa4cbaa4dc93470143d2e
fullhdpixel <tsmudde@hotmail.com>
2014-05-06 15:19:27 +0200

    Exercise 2 done (revisited)

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,4 +1,4 @@
 insert into F (x, y, violation, motivation) values
-	(ABC,EF,false,'BCNF is goed toegepast'),
-	(E,D,true,''),
+	(ABC,EF,false,'BCNF is goed toegepast. Als je ABC weet, kun je daar alles van afleiden (Superkey).'),
+	(E,D,true,'E -> is overbodig, want ABC -> EFD'),
 	(D,A,true,'Uit A volgt D d.m.v. ABC -> EF, E -> D, D -> A D is not unique');
