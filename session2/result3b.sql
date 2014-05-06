f298d9f63a18bb69bf5dde47fcf691066e47793c
fullhdpixel <tsmudde@hotmail.com>
2014-05-06 16:21:43 +0200

    Exercise 2.3b)
    Foreign key rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -5,3 +5,7 @@ SELECT p.pid FROM Person p, Movie m
 		AND m.mid = a.mid
 		AND m.name = 'Back to the Future'
 		);
+		
+SELECT DISTINCT a.pid FROM Acts a, Movie m
+	WHERE m.mid = a.mid
+	AND m.name = 'Back to the Future';
